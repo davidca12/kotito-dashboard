a81376f752f602e763ce41acc59120c2970d2c19
davidca12 <ca1david23@gmail.com>
2020-09-11 17:39:53 +0000

    primera parte

--- src/models.py
+++ src/models.py
@@ -4,9 +4,13 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name= db.Column(db.String(120), unique=True, nullable=False)
+    last_name=db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    #relacion
+    courses = db.relationship('Course', lazy=True)
+
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -17,3 +21,56 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+enrollment = db.Table('enrollment',
+    db.Column('course_id', db.Integer, db.ForeignKey('course.id'), primary_key=True),
+    db.Column('student_id', db.Integer, db.ForeignKey('student.id'), primary_key=True)
+)
+
+
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    kotokan_id = db.Column(db.String(120), unique=True, nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship("User") 
+
+    students = db.relationship("Student", secondary=enrollment, back_populates="enrollment")
+
+    
+
+
+    def __repr__(self):
+        return '<Course %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            # do not serialize the password, its a security breach
+        }
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    last_name= db.Column(db.String(120), unique=True, nullable=False)
+    kotokan_id = db.Column(db.String(120), unique=True, nullable=False)
+
+    #relacion muchos a muchos
+    courses = db.relationship("Course", secondary=enrollment, back_populates="enrollment")
+
+    def __repr__(self):
+        return '<Student %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "last_name": self.last_name
+            # do not serialize the password, its a security breach
+        }
+
+
+
